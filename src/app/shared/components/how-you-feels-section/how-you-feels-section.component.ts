d6cc117322923c4c0eeab7bf8178bfa89c19a2e6
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-08-03 15:27:34 -0300

    fix: Update how-you-feels-section component to use ToastrNotificationService for displaying success message

--- src/app/shared/components/how-you-feels-section/how-you-feels-section.component.ts
+++ src/app/shared/components/how-you-feels-section/how-you-feels-section.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Clipboard } from '@angular/cdk/clipboard';
-import { ToastrService } from 'ngx-toastr';
+import { ToastrNotificationService } from '../../../user-portal/services/toastr/toastr.service';
 
 @Component({
   selector: 'app-how-you-feels-section',
   standalone: true,
   imports: [ CommonModule ],
+  providers: [ Clipboard, ToastrNotificationService ],
   templateUrl: './how-you-feels-section.component.html',
   styleUrl: './how-you-feels-section.component.css'
 })
 export class HowYouFeelsSectionComponent {
 
-  constructor(private clipboard: Clipboard, private toastr: ToastrService) {}
+  constructor(private clipboard: Clipboard, private toastr: ToastrNotificationService) {}
 
   openChat() {
     // Open chat
@@ -22,16 +23,7 @@ export class HowYouFeelsSectionComponent {
     const number = '0800 000 0000';
 
     this.clipboard.copy(number);
-    this.toastr.show('Numero de telefone copiado com sucesso', 'Success', { timeOut: 6000,
-      progressBar: true,
-      closeButton: true,
-      positionClass: 'toast-top-center',
-      messageClass: 'toast-message',
-      tapToDismiss: true,
-      newestOnTop: true,
-      progressAnimation : 'decreasing',
-      
-    });
+    this.toastr.showSuccess('Numero de telefone copiado com sucesso', 'Success');
   }
 
   openScheduleSupportModal() {
