fee3f0f619de0dbefd7c2d21107b01b4cd39582a
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-08-04 12:36:17 -0300

    feat: Add loading indicator to "Enviar" button in be-a-volunteer-modal component

--- src/app/shared/components/be-a-volunteer-modal/be-a-volunteer-modal.component.ts
+++ src/app/shared/components/be-a-volunteer-modal/be-a-volunteer-modal.component.ts
@@ -5,6 +5,9 @@ import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ToastrNotificationService } from '../../../user-portal/services/toastr/toastr.service';
+import { EmailService } from '../../../simple-user/services/email/email.service';
+import { IEmail } from '../../../simple-user/interfaces/email/email.interface';
+import { FormaterMessage } from '../../../simple-user/utils/formater-message';
 
 @Component({
   selector: 'app-be-a-volunteer-modal',
@@ -24,8 +27,9 @@ import { ToastrNotificationService } from '../../../user-portal/services/toastr/
 export class BeAVolunteerModalComponent {
 
   contactForm! : FormGroup;
-    
-  constructor(public dialog: MatDialog, private toastr: ToastrNotificationService) {
+  btnIsLoading = false;
+
+  constructor(public dialog: MatDialog, private toastr: ToastrNotificationService, private emailService: EmailService) {
 
       this.contactForm = new FormGroup({
           name: new FormControl(''),
@@ -63,33 +67,98 @@ export class BeAVolunteerModalComponent {
     return true;
   }
 
-  submitForm() {
+  async submitForm() {
 
     if(!this.isFormValid()) {
       this.toastr.showError('Por favor, cheque todos os campos', 'Error');
       return;
     }
 
-    let email = {
-      message: this.getFormValue().message,
-      email: this.getFormValue().email,
-      title: 'Quero ser Voluntario',
-      name: this.getFormValue().name,
-      phoneNumber: this.getFormValue().phoneNumber,
-    }
+    let formValue = this.getFormValue();
+      let emailMessage = this.formatEmailMessage(formValue.message, formValue.name, formValue.phoneNumber, formValue.email);
 
-    this.sendEmail();
+      let email = {
+          to: formValue.email,
+          subject: 'Contato',
+          text: emailMessage,
+      } as IEmail;
+
+      await this.sendEmail(email);
   }
 
-  sendEmail() : Promise<void> {
+  async sendEmail(email: IEmail) : Promise<void> {
 
-    return new Promise((resolve, reject) => {
+    this.applyLoadingIndicator();
+    this.disableInputFieldAfterSubmit();
 
-      resolve(this.toastr.showSuccess('Email enviado com sucesso', 'Success'));
-      reject(this.toastr.showError('Erro ao enviar email', 'Error'));
+    await this.emailService.sendEmail(email)
+    .toPromise()
+    .then((response: HttpResponse<any> | any) => {
 
+      if(response.status !== 200) {
+        this.toastr.showError('Erro ao enviar email', 'Erro');
+        this.disableLoadingIndicator();
+        this.enableInputFieldAfterSubmit();
+        return;
+      }
 
-    });
+      if(response.status === 200 || response.status === 201) {
+        this.toastr.showSuccess('Email enviado com sucesso', 'Sucesso');
+        this.disableLoadingIndicator();
+        this.enableInputFieldAfterSubmit();
+      }
+
+    })
+    .catch((error: HttpErrorResponse) => {
+      this.toastr.showError('Erro ao enviar email', 'Erro');
+      this.disableLoadingIndicator();
+      this.enableInputFieldAfterSubmit();
+    })
+    
+  }
+
+  formatEmailMessage(message: string, name: string, phoneNumber: string, email: string) : string {
+    return FormaterMessage.formatEmailWantToBePartnerMessage(message, name, phoneNumber, email);
+  }
+
+  applyLoadingIndicator(): void {
+
+    this.btnIsLoading = true;
+
+  }
+
+  disableLoadingIndicator(): void {
+    
+    this.btnIsLoading = false;
+
+  }
+
+  disableInputFieldAfterSubmit(): void {
+
+    let inputName = document.getElementById('name') as HTMLInputElement;
+    let inputPhoneNumber = document.getElementById('phoneNumber') as HTMLInputElement;
+    let inputEmail = document.getElementById('email') as HTMLInputElement;
+    let inputMessage = document.getElementById('exampleFormControlTextarea1') as HTMLInputElement;
+
+    inputName.disabled = true;
+    inputPhoneNumber.disabled = true;
+    inputEmail.disabled = true;
+    inputMessage.disabled = true;
+
+  }
+
+  enableInputFieldAfterSubmit(): void {
+      
+    let inputName = document.getElementById('name') as HTMLInputElement;
+    let inputPhoneNumber = document.getElementById('phoneNumber') as HTMLInputElement;
+    let inputEmail = document.getElementById('email') as HTMLInputElement;
+    let inputMessage = document.getElementById('exampleFormControlTextarea1') as HTMLInputElement;
+
+    inputName.disabled = false;
+    inputPhoneNumber.disabled = false;
+    inputEmail.disabled = false;
+    inputMessage.disabled = false;
+
   }
 
 }
